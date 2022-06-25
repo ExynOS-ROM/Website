3d9e70b97b3f816b3cfd54a7a20c1110c18d484c
Kostas Tepetes <62772104+kostastepetes@users.noreply.github.com>
2022-06-25 20:42:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const kostasTexture = new THREE.TextureLoader().load('./kostas.png');
+const kostasTexture = new THREE.TextureLoader().load('C:\Users\user\Downloads\portfolio-3D-website-threejs-main\kostas.png');
 
 const kostas = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshBasicMaterial({ map: kostasTexture }));
 
